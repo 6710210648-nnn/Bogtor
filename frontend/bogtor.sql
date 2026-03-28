39f152242a90b95b1864e027ae93f6149862363c
nusteekai <6710210648@psu.ac.th>
2026-03-28 20:15:19 +0700

    ข้อมูล

--- frontend/bogtor.sql
+++ frontend/bogtor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2026 at 07:06 PM
+-- Generation Time: Mar 22, 2026 at 08:09 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -77,7 +77,7 @@ INSERT INTO `travel` (`id`, `title_th`, `title_en`, `description`, `location_add
 (4, 'วัดหาดใหญ่ใน', 'Mahattamangkalaram', 'ตลาดน้ำแห่งแรกของภาคใต้ สัมผัสวิถีชีวิตชาวบ้าน ชิมอาหารท้องถิ่นที่ขายบนเรือพาย และใช้ภาชนะจากธรรมชาติ', 'ถนนเพชรเกษม อำเภอหาดใหญ่ จังหวัดสงขลา', '', '07:00 - 18:00 น.', '/วัดหาดใหญ่ใน.jpg', 'ศาสนา'),
 (5, 'น้ำตกโตนงาช้าง', 'Ton Nga Chang Waterfall', 'น้ำตกที่มีชื่อเสียงระดับจังหวัด มีทั้งหมด 7 ชั้น โดยเฉพาะชั้นที่ 3 (งาช้าง) ที่มีความสวยงามเป็นพิเศษ', 'เขตรักษาพันธุ์สัตว์ป่าโตนงาช้าง อำเภอหาดใหญ่ จังหวัดสงขลา', '', '09:00 - 16:00 น.', '/โตนงาช้าง.jpg', 'ธรรมชาติ'),
 (6, 'เซ็นทรัล หาดใหญ่', 'Central Hatyai', 'ห้างสรรพสินค้าที่ใหญ่และทันสมัยที่สุดในหาดใหญ่ ครบครันด้วยสินค้าและบริการ', 'ถนนกาญจนวณิช อำเภอหาดใหญ่ จังหวัดสงขลา', '', '10:00 - 21:00 น.', '/เซนทรัล.jpg', 'ตลาด/ช้อปปิ้ง'),
-(7, 'มัสยิดกลางประจำจังหวัดสงขลา', 'Songkhla Central Mosque', 'ศาสนสถานอันวิจิตรงดงาม เป็นศูนย์รวมจิตใจของชาวมุสลิมและเป็นจุดถ่ายภาพยอดนิยม', 'ตำบลคลองแห อำเภอหาดใหญ่ จังหวัดสงขลา', '', '08:30 - 15:30 น.', '/mussayid.jpg', 'ศาสนา'),
+(7, 'มัสยิดกลางประจำจังหวัดสงขลา', 'Songkhla Central Mosque', 'ศาสนสถานอันวิจิตรงดงาม เป็นศูนย์รวมจิตใจของชาวมุสลิมและเป็นจุดถ่ายภาพยอดนิยม', 'ตำบลคลองแห อำเภอหาดใหญ่ จังหวัดสงขลา', '', '08:30 - 15:30 น.', '', 'ศาสนา'),
 (8, 'วัดฉื่อฉาง', 'Tse Chang Temple', 'วัดจีนที่มีสถาปัตยกรรมโดดเด่น และเป็นศูนย์กลางการจัดงานเทศกาลสำคัญของชาวไทยเชื้อสายจีน', 'ถนนศุภสารรังสรรค์ อำเภอหาดใหญ่ จังหวัดสงขลา', '', '07:00 - 19:00 น.', '/ฉือฉาง.jpg', 'ศาสนา'),
 (9, 'ตลาดกรีนเวย์ไนท์มาร์เก็ต', 'Greenway Night Market', 'ตลาดนัดกลางคืนยอดนิยม แหล่งรวมเสื้อผ้าแฟชั่น สินค้ามือสอง และโซนอาหารที่หลากหลาย', 'ถนนกาญจนวณิชย์ อำเภอหาดใหญ่ จังหวัดสงขลา', '', '17:00 - 22:00 น.', '/กรีนเว.jpg', 'ตลาด/ช้อปปิ้ง'),
 (10, 'พระมหาธาตุเจดีย์ไตรภพไตรมงคล', 'Maha That Chedi Triphop Tri Mongkhon', 'เจดีย์สแตนเลส (วัดสแตนเลส) ตั้งอยู่บนเขาคอหงส์ ภายในมีการจัดแสดงภาพวาดสามมิติและงานพุทธศิลป์', 'ถนนปุณณกัณฑ์ ตำบลคอหงส์ อำเภอ', '', 'เปิด 24 ชั่วโมง', '/jeady.jpg', 'ศาสนา');
@@ -92,7 +92,6 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
   `gender` enum('male','female') DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `image_url` varchar(255) DEFAULT NULL
@@ -102,17 +101,20 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `gender`, `phone`, `image_url`) VALUES
-(1, 'Somchai', 'somchai@gmail.com', '1234', 'male', '0861111111', NULL),
-(2, 'Suda', 'suda@gmail.com', '1234', 'female', '0862222222', NULL),
-(3, 'Anan', 'anan@gmail.com', '1234', 'male', '0863333333', NULL),
-(4, 'Kanya', 'kanya@gmail.com', '1234', 'female', '0864444444', NULL),
-(5, 'Prasert', 'prasert@gmail.com', '1234', 'male', '0865555555', NULL),
-(6, 'Malee', 'malee@gmail.com', '1234', 'female', '0866666666', NULL),
-(7, 'Narin', 'narin@gmail.com', '1234', 'male', '0867777777', NULL),
-(8, 'Orn', 'orn@gmail.com', '1234', 'female', '0868888888', NULL),
-(9, 'Wichai', 'wichai@gmail.com', '1234', 'male', '0869999999', NULL),
-(10, 'Pim', 'pim@gmail.com', '1234', 'female', '0870000000', NULL);
+INSERT INTO `users` (`id`, `name`, `email`, `gender`, `phone`, `image_url`) VALUES
+(1, 'Somchai', 'somchai@gmail.com', 'male', '0861111111', 'http://localhost:3001/uploads/1774162527613.webp'),
+(2, 'Suda', 'suda@gmail.com', 'female', '0862222222', 'http://localhost:3001/uploads/1774162580518.jpg'),
+(3, 'Anan', 'anan@gmail.com', 'male', '0863333333', 'http://localhost:3001/uploads/1774162599606.jpg'),
+(4, 'Kanya', 'kanya@gmail.com', 'female', '0864444444', 'http://localhost:3001/uploads/1774162608615.jpg'),
+(5, 'Prasert', 'prasert@gmail.com', 'male', '0865555555', 'http://localhost:3001/uploads/1774162621945.jpg'),
+(6, 'Malee', 'malee@gmail.com', 'female', '0866666666', 'http://localhost:3001/uploads/1774162628012.jpg'),
+(7, 'Narin', 'narin@gmail.com', 'male', '0867777777', 'http://localhost:3001/uploads/1774162636267.jpg'),
+(8, 'Orn', 'orn@gmail.com', 'female', '0868888888', 'http://localhost:3001/uploads/1774162645670.jpeg'),
+(9, 'Wichai', 'wichai@gmail.com', 'male', '0869999999', 'http://localhost:3001/uploads/1774162654003.jpg'),
+(10, 'Pim', 'pim@gmail.com', 'female', '0870000000', 'http://localhost:3001/uploads/1774162663917.jpg'),
+(11, 'yam', 'yam@email.com', 'female', '0875401690', 'http://localhost:3001/uploads/1774162687952.webp'),
+(12, 'nusteekai', 'nnn648@gmail.com', 'female', '0987024598', 'http://localhost:3001/uploads/1774163037325.png'),
+(13, 'nusraknat', '', 'female', '0000000000', 'http://localhost:3001/uploads/1774163104871.jpg');
 
 --
 -- Indexes for dumped tables
@@ -136,6 +138,7 @@ ALTER TABLE `travel`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -152,7 +155,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
